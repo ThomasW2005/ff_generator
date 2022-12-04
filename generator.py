09aa97e20ac27f87dc3abb56c3a662aa4c6d1cba
Thomas Weichhart <multihacker007yt@gmail.com>
2022-12-04 18:16:41 +0100

    version 1.0

--- generator.py
+++ generator.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import customtkinter as ctk
+import weakref
+from templates import *
+
+##newlines to br in bericht
 
 
 class FahrzeugeFrame(ctk.CTkFrame):
@@ -12,26 +16,164 @@ class FahrzeugeFrame(ctk.CTkFrame):
         self.vf = tk.BooleanVar()
         self.klf = tk.BooleanVar()
         self.wlf = tk.BooleanVar()
-        
+
         self.grid_columnconfigure((0, 1, 2, 3, 4, 5), weight=1)  # sort of sticky="ew"
+
         self.fahrzeuge_tlf = ctk.CTkCheckBox(self, text="TLF", variable=self.tlf).grid(row=0, column=0, padx=5, pady=10)
-        self.fahrzeuge_rs = ctk.CTkCheckBox(self, text="RS", variable=self.rs).grid(row=0, column=1, padx=5, pady=10)
+        self.fahrzeuge_rs = ctk.CTkCheckBox(self, text="RF-S", variable=self.rs).grid(row=0, column=1, padx=5, pady=10)
         self.fahrzeuge_mtf = ctk.CTkCheckBox(self, text="MTF", variable=self.mtf).grid(row=0, column=2, padx=5, pady=10)
         self.fahrzeuge_vf = ctk.CTkCheckBox(self, text="VF", variable=self.vf).grid(row=0, column=3, padx=5, pady=10)
         self.fahrzeuge_klf = ctk.CTkCheckBox(self, text="KLF", variable=self.klf).grid(row=0, column=4, padx=5, pady=10)
         self.fahrzeuge_wlf = ctk.CTkCheckBox(self, text="WLF", variable=self.wlf).grid(row=0, column=5, padx=5, pady=10)
-        self.grid(row=10, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
 
     def get_fahrzeuge(self):
-        return self.tlf.get(), self.rs.get(), self.mtf.get(), self.vf.get(), self.klf.get(), self.wlf.get()
-
-    def print_test(self):
-        print("test")
+        fahrzeuge = []
+        if self.tlf.get():
+            fahrzeuge.append("TLF")
+        if self.rs.get():
+            fahrzeuge.append("RF-S")
+        if self.mtf.get():
+            fahrzeuge.append("MTF")
+        if self.vf.get():
+            fahrzeuge.append("VF")
+        if self.klf.get():
+            fahrzeuge.append("KLF")
+        if self.wlf.get():
+            fahrzeuge.append("WLF")
+        return fahrzeuge
 
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+
+        self.title("FF-Bhk Einsatzgenerator")
+        self.geometry("950x880")
+
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.generator_entry = Generator(self)
+        self.generator_entry.grid(row=0, column=0, sticky="news", padx=10, pady=10)
+
+        self.output_entry = ctk.CTkTextbox(self, width=600)
+        self.output_entry.grid(row=0, column=1, sticky="news", padx=(0, 10), pady=10)
+
+        self.button_generate = ctk.CTkButton(self.generator_entry, text="Generate", command=self.generate).grid(row=15, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
+
+    def write_out(self, data):
+        t = tk.Tk()
+        t.withdraw()
+        t.clipboard_clear()
+        t.clipboard_append(data)
+        t.update()
+        t.destroy()
+
+        self.output_entry.delete(1.0, tk.END)
+        self.output_entry.insert(tk.END, data)
+
+    def generate(self):
+        date = self.generator_entry.date.get().replace(".", "")
+        date_with_dots = self.generator_entry.date.get()
+        if len(date) != 8:
+            self.write_out("Fehler: Datum ist nicht korrekt")
+            return
+
+        if (not self.generator_entry.type.get()) and (not self.generator_entry.level.get()):
+            self.write_out("Fehler: Kein Einsatztyp ausgewählt")
+            return
+        type = self.generator_entry.type.get()[0] + str(self.generator_entry.level.get())
+
+        if type == "T4" or type == "S4":
+            self.write_out("Fehler: Einsatzstufe 4 ist nicht für Technische oder Schadstoffeinsätze möglich")
+            return
+
+        meldebild = self.generator_entry.einsatz_meldebild.get()
+        if not meldebild:
+            self.write_out("Fehler: Kein Meldebild ausgewählt")
+            return
+
+        uhrzeit = self.generator_entry.uhrzeit.get()
+        if len(uhrzeit) != 5:
+            self.write_out("Fehler: Uhrzeit ist nicht korrekt")
+            return
+
+        ort = self.generator_entry.einsatzort.get()
+        if not ort:
+            self.write_out("Fehler: Kein Einsatzort angegeben")
+            return
+
+        alarmierung = ""
+        if not self.generator_entry.pager.get() and not (self.generator_entry.sirene.get()):
+            self.write_out("Fehler: Keine Alarmierung ausgewählt")
+            return
+
+        if self.generator_entry.pager.get():
+            alarmierung += temp_pager
+        if self.generator_entry.sirene.get():
+            alarmierung += temp_sirene
+
+        fahrzeuge_bild = ""
+        fahrzeuge_text = ""
+        fahrzeuge = self.generator_entry.fahrzeuge_entry.get_fahrzeuge()
+        if not fahrzeuge:
+            self.write_out("Fehler: Keine Fahrzeuge ausgewählt")
+            return
+
+        for fahrzeug in fahrzeuge:
+            fahrzeuge_text += temp_auto_text_template.replace("[--fahrzeug--]", fahrzeug)
+            if fahrzeug == "TLF":
+                fahrzeuge_bild += temp_auto_tlf
+            elif fahrzeug == "RF-S":
+                fahrzeuge_bild += temp_auto_rf
+            elif fahrzeug == "MTF":
+                fahrzeuge_bild += temp_auto_mtf
+            elif fahrzeug == "VF":
+                fahrzeuge_bild += temp_auto_vf
+            elif fahrzeug == "KLF":
+                fahrzeuge_bild += temp_auto_klf
+            elif fahrzeug == "WLF":
+                fahrzeuge_bild += temp_auto_wlf
+
+        eingesetzte_menner = int(self.generator_entry.eingesetzte_menner.get())
+
+        if not eingesetzte_menner:
+            self.write_out("Fehler: Keine eingesetzten Männer angegeben")
+            return
+
+        andere_beteiligte = ""
+        unsere_ff = f'FF Böheimkirchen ‑ Markt ({len(fahrzeuge)} Fahrzeug{"" if len(fahrzeuge)==1 else "e"} + {eingesetzte_menner} Mann ‑ inkl. Bereitschaft)'
+        andere_beteiligte += temp_andere_beteiligte.replace("[--andere--beteiligte--]", unsere_ff)
+
+        andere_beteiligte_read = self.generator_entry.andere_beteiligte.get()
+        if andere_beteiligte_read:
+            for andere in andere_beteiligte_read.splitlines():
+                andere_beteiligte += temp_andere_beteiligte.replace("[--andere--beteiligte--]", andere)
+
+        bericht = self.generator_entry.einsatzbericht.get()
+        if not bericht:
+            self.write_out("Fehler: Kein Einsatzbericht angegeben")
+            return
+
+        template = open("template_do_not_touch.html", "r", encoding="utf-8")
+        output = template.read()
+        output = output.replace("[--insert--date--here--]", date)
+        output = output.replace("[--insert--type--here--]", type)
+        output = output.replace("[--insert--meldebild--here--]", meldebild)
+        output = output.replace("[--insert--date--with--dots--here--]", date_with_dots)
+        output = output.replace("[--insert--uhrzeit--here--]", uhrzeit)
+        output = output.replace("[--insert--ort--here--]", ort)
+        output = output.replace("[--insert--alarmierung--here--]", alarmierung)
+        output = output.replace("[--insert--fahrzeuge--text--here--]", fahrzeuge_text)
+        output = output.replace("[--insert--fahrzeuge--bild--here--]", fahrzeuge_bild)
+        output = output.replace("[--insert--bericht--here--]", bericht.replace("\n", "<br />"))
+        output = output.replace("[--insert--andere--beteiligte--here--]", andere_beteiligte)
+
+        self.write_out(output)
+
+
+class Generator(ctk.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.date = tk.StringVar()
         self.uhrzeit = tk.StringVar()
@@ -41,15 +183,19 @@ class App(ctk.CTk):
         self.sirene = tk.BooleanVar()
         self.type = tk.StringVar()
         self.level = tk.IntVar()
-
-        self.title("FF-Bhk Einsatzgenerator")
-        self.geometry("650x800")
+        self.andere_beteiligte = tk.StringVar()
+        self.einsatzbericht = tk.StringVar()
+        self.eingesetzte_menner = tk.StringVar()
 
         self.grid_rowconfigure(0, pad=10)
         self.grid_columnconfigure((0, 1), weight=1, pad=10)
 
-        self.app_title = ctk.CTkLabel(self, text="FF-Generator", font=("Arial", 20)).grid(row=0, column=0, sticky="ew", padx=10, pady=10)
-        self.button_generate = ctk.CTkButton(self, text="Generate", command=self.generate).grid(row=0, column=1, sticky="ew", padx=10, pady=10)
+        # self.app_title = ctk.CTkLabel(self, text="FF-Generator", font=("Arial", 20)).grid(row=0, column=0, sticky="ew", padx=10, pady=10)
+        # self.button_generate = ctk.CTkButton(self, text="Generate", command=self.generate).grid(row=0, column=1, sticky="ew", padx=10, pady=10)
+
+        # anzahl der eingesetzten männer
+        self.eingesetzte_menner_label = ctk.CTkLabel(self, text="Eingesetzte Männer:").grid(row=0, column=0, sticky="we", padx=10, pady=10)
+        self.eingesetzte_menner_entry = ctk.CTkEntry(self, textvariable=self.eingesetzte_menner).grid(row=0, column=1, sticky="ew", padx=10, pady=10)
 
         ## date
         self.date_label = ctk.CTkLabel(self, text="Datum:").grid(row=1, column=0, padx=10, pady=2)
@@ -78,206 +224,21 @@ class App(ctk.CTk):
 
         # fahrzeuge
         self.fahrzeuge_label = ctk.CTkLabel(self, text="Eingesetzte Fahrzeuge:").grid(row=9, column=0, columnspan=2, padx=10, pady=2)
-        self.fahrzeuge_entry = FahrzeugeFrame(self).grid(row=10, column=0, columnspan=2, padx=10, pady=2, sticky="ew")
+        self.fahrzeuge_entry = FahrzeugeFrame(self)
+        self.fahrzeuge_entry.grid(row=10, column=0, columnspan=2, padx=10, pady=2, sticky="ew")
 
-    def generate(self):
-        print(self.date.get())
-        print(self.uhrzeit.get())
-        print(self.einsatzort.get())
-        print(self.einsatz_meldebild.get())
-        print(self.pager.get())
-        print(self.sirene.get())
-        # print(self.type.get())
-        # print(self.level.get())
-        print(self.type.get()[0] + str(self.level.get()))
-        # print(self.fahrzeuge_entry.get_fahrzeuge())
-        print(self.fahrzeuge_entry.print_test())
-        print(self.dataa)
+        # andere beteiligte
+        self.beteiligte_label = ctk.CTkLabel(self, text="Andere Beteiligte:").grid(row=11, column=0, columnspan=2, padx=10, pady=2)
+        self.beteiligte_entry = ctk.CTkTextbox(self, height=100, font=("Arial", 12))
+        self.beteiligte_entry.bind("<KeyRelease>", lambda event: self.andere_beteiligte.set(event.widget.get("1.0", "end-1c")))
+        self.beteiligte_entry.grid(row=12, column=0, columnspan=2, sticky="ew", padx=10, pady=2)
 
+        # einsatzbericht
+        self.einsatzbericht_label = ctk.CTkLabel(self, text="Einsatzbericht:").grid(row=13, column=0, columnspan=2, padx=10, pady=2)
+        self.einsatzbericht_entry = ctk.CTkTextbox(self, font=("Arial", 12))
+        self.einsatzbericht_entry.bind("<KeyRelease>", lambda event: self.einsatzbericht.set(event.widget.get("1.0", "end-1c")))
+        self.einsatzbericht_entry.grid(row=14, column=0, columnspan=2, sticky="news", padx=10, pady=10)
 
-# class fahrzeuge:
-#     tlf = False
-#     rs = False
-#     mtf = False
-#     vf = False
-#     klf = False
-#     wlf = False
-
-#     def is_valid():
-#         if fahrzeuge.tlf or fahrzeuge.rs or fahrzeuge.mtf or fahrzeuge.vf or fahrzeuge.klf or fahrzeuge.wlf:
-#             return True
-#         else:
-#             return False
-
-
-# class alarmierung:
-#     sirene = False
-#     pager = False
-
-#     def is_valid():
-#         return alarmierung.sirene | alarmierung.pager
-
-
-# class einsatz_art:
-#     type = None
-#     level = None
-
-#     def is_valid():
-#         val = True
-
-#         if einsatz_art.type == "":
-#             val = False
-#         if einsatz_art.level == 0:
-#             val = False
-#         if einsatz_art.level == 4 and (einsatz_art.type[0] == "S" or einsatz_art.type[0] == "T"):
-#             val = False
-
-#         return val
-
-#     def get_short():
-#         return (einsatz_art.type[0] if einsatz_art.type else "") + str(einsatz_art.level)
-
-
-# def generate():
-#     tmp_date = date_picked.get().replace(".", "")
-#     date_picked.valid = tmp_date.isdigit() and len(tmp_date) == 8
-
-#     alarmierung.sirene = sirene_used.get()
-#     alarmierung.pager = pager_used.get()
-
-#     einsatz_art.type = einsatz_type.get()
-#     einsatz_art.level = einsatz_level.get()
-
-#     einsatz_meldebild.valid = einsatz_meldebild.get() != ""
-
-#     uhrzeit.valid = uhrzeit.get() != ""
-
-#     einsatzort.valid = einsatzort.get() != ""
-
-#     fahrzeuge.tlf = tlf_used.get()
-#     fahrzeuge.rs = rs_used.get()
-#     fahrzeuge.mtf = mtf_used.get()
-#     fahrzeuge.vf = vf_used.get()
-#     fahrzeuge.klf = klf_used.get()
-#     fahrzeuge.wlf = wlf_used.get()
-
-#     bericht.valid = bericht.get() != ""
-
-#     andere_beteiligte.valid = andere_beteiligte.get() != ""
-
-#     print(f'{"🟢" if date_picked.valid else "❌"} Date: {date_picked.get()}')
-#     print(f'{"🟢" if uhrzeit.valid else "❌"} Uhrzeit: {uhrzeit.get()}')
-#     print(f'{"🟢" if einsatzort.valid else "❌"} Einsatzort: {einsatzort.get()}')
-#     print(f'{"🟢" if alarmierung.is_valid() else "❌"} Alarmierungsart: sirene={alarmierung.sirene}, pager={alarmierung.pager}')
-#     print(f'{"🟢" if einsatz_art.is_valid() else "❌"} Einsatzart: {einsatz_art.get_short()}')
-#     print(f'{"🟢" if einsatz_meldebild.valid else "❌"} Meldebild: {einsatz_meldebild.get()}')
-#     print(f'{"🟢" if fahrzeuge.is_valid() else "❌"} Fahrzeuge: tlf={fahrzeuge.tlf}, rs={fahrzeuge.rs}, mtf={fahrzeuge.mtf}, vf={fahrzeuge.vf}, klf={fahrzeuge.klf}, wlf={fahrzeuge.wlf}')
-#     print(f'{"🟢" if bericht.valid else "❌"} Bericht: {bericht.get()}')
-#     print(f'{"🟢" if andere_beteiligte.valid else "❌"} Andere Beteiligte: {andere_beteiligte.get()}')
-
-#     all_valid = (
-#         date_picked.valid and alarmierung.is_valid() and einsatz_art.is_valid() and einsatz_meldebild.valid and uhrzeit.valid and einsatzort.valid and fahrzeuge.is_valid() and bericht.valid and andere_beteiligte.valid
-#     )
-#     print(f'{"🟢" if all_valid else "❌"}')
-
-#     print("")
-
-
-# # bericht, andere leute
-
-# ###setup
-# root = ctk.CTk()
-# root.geometry("650x800")
-# root.title("FF-Generator")
-# date_picked = tk.StringVar()
-# einsatz_type = tk.StringVar()
-# sirene_used = tk.BooleanVar()
-# pager_used = tk.BooleanVar()
-# einsatz_level = tk.IntVar()
-# einsatz_meldebild = tk.StringVar()
-# uhrzeit = tk.StringVar()
-# einsatzort = tk.StringVar()
-# bericht = tk.StringVar()
-# andere_beteiligte = tk.StringVar()
-
-# tlf_used = tk.BooleanVar()
-# rs_used = tk.BooleanVar()
-# mtf_used = tk.BooleanVar()
-# vf_used = tk.BooleanVar()
-# klf_used = tk.BooleanVar()
-# wlf_used = tk.BooleanVar()
-
-# ### gloabl ui
-
-# root.grid_rowconfigure(0, pad=10)
-# root.grid_columnconfigure(0, weight=1)
-# root.grid_columnconfigure(1, weight=1)
-
-# label = ctk.CTkLabel(root, text="FF-Generator", font=("Arial", 20)).grid(row=0, column=0, sticky="ew", padx=10, pady=10)
-
-# ###generate button
-# button_generate = ctk.CTkButton(root, text="Generate", command=generate).grid(row=0, column=1, sticky="ew", padx=10, pady=10)
-
-# ### date
-# date_text = ctk.CTkLabel(root, text="Datum:").grid(row=1, column=0, padx=10, pady=2)
-# date_entry = ctk.CTkEntry(root, textvariable=date_picked).grid(row=2, column=0, sticky="ew", padx=10, pady=2)
-
-
-# ### uhrzeit
-# uhrzeit_text = ctk.CTkLabel(root, text="Uhrzeit:").grid(row=1, column=1, padx=10, pady=2)
-# uhrzeit_entry = ctk.CTkEntry(root, textvariable=uhrzeit).grid(row=2, column=1, sticky="ew", padx=10, pady=2)
-
-
-# ### einsatzort
-# einsatzort_text = ctk.CTkLabel(root, text="Einsatzort:").grid(row=3, column=0, padx=10, pady=2)
-# einsatzort_entry = ctk.CTkEntry(root, textvariable=einsatzort).grid(row=4, column=0, sticky="ew", padx=10, pady=2)
-
-
-# ### meldebild
-# meldebild_text = ctk.CTkLabel(root, text="Meldebil des Einsatzes:").grid(row=3, column=1, padx=10, pady=2)
-# meldebild_entry = ctk.CTkEntry(root, textvariable=einsatz_meldebild).grid(row=4, column=1, sticky="ew", padx=10, pady=2)
-
-
-# # ### alarmierung
-# ctk.CTkLabel(root, text="Alarmierung:").grid(row=5, column=0, columnspan=2, padx=10, pady=2)
-# checkbox_sirene = ctk.CTkCheckBox(root, text="Sirene", variable=sirene_used).grid(row=6, column=0, padx=10, pady=2)
-# checkbox_pager = ctk.CTkCheckBox(root, text="Pager", variable=pager_used).grid(row=6, column=1, padx=10, pady=2)
-
-
-# ### art des einsatzes
-# checkbox_type = ctk.CTkSegmentedButton(root, variable=einsatz_type, values=["Brandeinsatz (B)", "Technischer Einsatz (T)", "Schadstoffeinsatz (S)"]).grid(row=7, column=0, columnspan=2, padx=10, pady=10)
-# checkbox_level = ctk.CTkSegmentedButton(root, variable=einsatz_level, values=[1, 2, 3, 4]).grid(row=8, column=0, columnspan=2, padx=10, pady=10)
-
-
-# ### fahrzeuge
-# fahrzeuge_text = ctk.CTkLabel(root, text="Eingesetzte Fahrzeuge:").grid(row=9, column=0, columnspan=2, padx=10, pady=2)
-# FahrzeugeFrame = ctk.CTkFrame(root)
-# FahrzeugeFrame.grid_columnconfigure((0, 1, 2, 3, 4, 5), weight=1)  # sort of sticky="ew"
-# FahrzeugeFrame.fahrzeuge_tlf = ctk.CTkCheckBox(FahrzeugeFrame, text="TLF", variable=tlf_used).grid(row=0, column=0, padx=5, pady=10)
-# FahrzeugeFrame.fahrzeuge_rs = ctk.CTkCheckBox(FahrzeugeFrame, text="RS", variable=rs_used).grid(row=0, column=1, padx=5, pady=10)
-# FahrzeugeFrame.fahrzeuge_mtf = ctk.CTkCheckBox(FahrzeugeFrame, text="MTF", variable=mtf_used).grid(row=0, column=2, padx=5, pady=10)
-# FahrzeugeFrame.fahrzeuge_vf = ctk.CTkCheckBox(FahrzeugeFrame, text="VF", variable=vf_used).grid(row=0, column=3, padx=5, pady=10)
-# FahrzeugeFrame.fahrzeuge_klf = ctk.CTkCheckBox(FahrzeugeFrame, text="KLF", variable=klf_used).grid(row=0, column=4, padx=5, pady=10)
-# FahrzeugeFrame.fahrzeuge_wlf = ctk.CTkCheckBox(FahrzeugeFrame, text="WLF", variable=wlf_used).grid(row=0, column=5, padx=5, pady=10)
-# FahrzeugeFrame.grid(row=10, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
-
-
-# ### andere beteiligte
-# beteiligte_text = ctk.CTkLabel(root, text="Andere Beteiligte:").grid(row=11, column=0, columnspan=2, padx=10, pady=2)
-# # beteiligte_entry = ctk.CTkEntry(root, textvariable=andere_beteiligte, placeholder_text="CTkEntry").grid(row=12, column=0, columnspan=2, sticky="ew", padx=10, pady=2)
-# beteiligte_entry = tk.Text(root, height=5, font=("Arial", 12), wrap="word", background="darkgray")
-# beteiligte_entry.bind("<KeyRelease>", lambda event: andere_beteiligte.set(event.widget.get("1.0", "end-1c")))
-# beteiligte_entry.grid(row=12, column=0, columnspan=2, sticky="ew", padx=10, pady=2)
-
-
-# # ### bericht
-# bericht_text = ctk.CTkLabel(root, text="Bericht:").grid(row=13, column=0, columnspan=2, padx=10, pady=2)
-# bericht_entry = tk.Text(root, height=10, font=("Arial", 12), wrap="word", background="darkgray")
-# bericht_entry.bind("<KeyRelease>", lambda event: bericht.set(event.widget.get("1.0", "end-1c")))
-# bericht_entry.grid(row=14, column=0, columnspan=2, sticky="ew", padx=10, pady=2)
-
-
-# root.mainloop()
 
 if __name__ == "__main__":
     app = App()
---
+++ templates.py
@@ -0,0 +1,53 @@
+temp_pager = """<td>
+                                <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=100x10000:format=png/path/s47c677f812c00ba1/image/i0b7521ebe95b9430/version/1470063035/image.png"
+                                    alt="" />
+                            </td>"""
+
+temp_sirene = """<td>
+                                <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=100x10000:format=png/path/s47c677f812c00ba1/image/i5946598134795bc9/version/1470069114/image.png"
+                                    alt="" />
+                            </td>"""
+
+temp_auto_tlf = """<td>
+                <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/tlf-a/"><img
+                        src="https://image.jimcdn.com/app/cms/image/transf/dimension=118x10000:format=png/path/s47c677f812c00ba1/image/i65e49bdd863c0ab8/version/1446024823/image.png"
+                        alt="" /></a>
+            </td>"""
+
+temp_auto_rf = """<td>
+                <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/rf-s/"><img
+                        src="https://image.jimcdn.com/app/cms/image/transf/dimension=125x10000:format=png/path/s47c677f812c00ba1/image/i02b8c065ba22cf22/version/1446024826/image.png"
+                        alt="" /></a>
+            </td>"""
+
+temp_auto_mtf = """<td>
+                <a href="https://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/mtf/"><img
+                        src="https://image.jimcdn.com/app/cms/image/transf/dimension=110x10000:format=jpg/path/s47c677f812c00ba1/image/i2a56691932c87b5c/version/1502553907/image.jpg"
+                        alt="" /></a>
+            </td>"""
+
+temp_auto_vf = """<td>
+                <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/vf/"><img
+                        src="https://image.jimcdn.com/app/cms/image/transf/dimension=105x10000:format=png/path/s47c677f812c00ba1/image/i2ad687b46b32d5a8/version/1446024814/image.png"
+                        alt="" /></a>
+            </td>"""
+
+temp_auto_klf = """<td>
+                <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/klf/"><img
+                        src="https://image.jimcdn.com/app/cms/image/transf/dimension=95x10000:format=png/path/s47c677f812c00ba1/image/ia8690be94128764c/version/1446024810/image.png"
+                        alt="" /></a>
+            </td>"""
+
+temp_auto_wlf = """<td>
+                <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/wlf/"><img
+                        src="https://image.jimcdn.com/app/cms/image/transf/dimension=118x10000:format=png/path/s47c677f812c00ba1/image/ia27f06a7a02a154f/version/1446024218/image.png"
+                        alt="" /></a>
+            </td>"""
+
+temp_auto_text_template = """<td align="center">
+                <font size="+1"><strong>[--fahrzeug--]</strong></font>
+            </td>"""
+
+temp_andere_beteiligte = """ <li>
+                        <font size="+1">[--andere--beteiligte--]</font>
+                    </li>"""
